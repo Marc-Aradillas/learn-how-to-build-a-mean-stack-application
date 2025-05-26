0b9ce3852227d670890b5dd147b6966b33490bdc
Marc Aradillas <aradillasm@gmail.com>
2025-05-25 23:48:10 -0500

    week 2 items

--- server/datasources.development.js
+++ server/datasources.development.js
@@ -1,11 +1,9 @@
 module.exports = {
- mongodb: {
-   connector: 'mongodb',
-   hostname: process.env.DB_HOST,
-   port: process.env.DB_PORT,
-   user: process.env.DB_USER,
-   password: process.env.DB_PASSWORD,
-   database: process.env.DB_NAME,
-   url: process.env.DB_URL
- }
+  mongodb: {
+    name: 'mongodb',
+    connector: 'mongodb',
+    hostname: process.env.DB_HOST || 'localhost',
+    port: 27017,
+    database: process.env.DB_NAME || 'lafs'
+  }
 };
